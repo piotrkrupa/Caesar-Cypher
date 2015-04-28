91215478e042eb29aecf573405ef493b3e71ea22
pitjdm <piotr.krupa91@gmail.com>
2015-04-28 11:03:16 +0200

    nowe

--- Source.c
+++ Source.c
@@ -1,49 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define max_tab 50
 
 //
 //a=97, z=122
 //A=65, Z=90
 
-int cezar(char *tab, int p, int size){
+void Cesar(char *tab, int move, int size){
 	int i, temp;
 	for( i = 0; i < size; i++){
-		//printf("%c", tab[i]);
-		printf("%c", tab[i] +p);
-	}
-
-	for( i = 0; i < size; i++){
-		if( tab[i] >= 97 && tab[i] <= 122){
-			if(tab[i] > 122){
-				temp = tab[i] - 122;
+		if( tab[i] >= 97 && tab[i] <= 122 ){
+			if( tab[i] >= 97 && tab[i] <= 122 ){
+				tab[i] = tab[i] + move; // temp - probability of ASCII overflow (only 0-127)
 			}
-			tab[i] = tab[i]+p;
+			if( tab[i] > 122){
+				tab[i] = tab[i] - 26;
+			}
+
 		}
 
-
-
-
-		if( tab[i] >= 65 && tab[i] <= 90){
-			tab[i] = tab[i]+p;
+		if( tab[i] >= 65 && tab[i] <= 90 ){
+			if( tab[i] >= 65 && tab[i] <= 90 ){
+				tab[i] = tab[i] + move;
+			}
+			if( tab[i] > 90 ){
+				tab[i] = tab[i] - 26;
+			}
 		}
 	}
-
-	return 0;
-
+}
+void CesarDecryption(char *tab, int move, int size){
+	int i;
+	for( i = 0; i < size; i++){
+		if( tab[i] >= 97 && tab[i] <= 122 ){
+			if( tab[i] >= 97 && tab[i] <= 122 ){
+				tab[i] = tab[i] - move;
+			}
+			if ( tab[i] < 97 ){
+				tab[i] = tab[i] + 26;
+			}
+		}
+		if( tab[i] >= 65 && tab[i] <= 90 ){
+			if( tab[i] >= 65 && tab[i] <= 90 ){
+				tab[i] = tab[i] - move;
+			}
+			if ( tab[i] < 65 ){
+				tab[i] = tab[i] + 26;
+			}
+		}
+	}
 }
 
-
-
 int main(){
-	int p;
-	int size;
-	char tab[] = {"AaaaaBbbbbCcccc"};
-	p=1;
-	size = sizeof(tab);
+	int p, move, i;
+	char tab[max_tab], *ptab;
+	
+	p=27; //move factor, p>0 !
+	//move= p%26; 
+	move=18;
+	printf("p: %d\nmove: %d\n", p, move);
 
-	cezar(&tab[0], p, size);
+	ptab = fgets(tab, max_tab, stdin);
 
+	printf("\nString(original): ");
+	for( i = 0; i < max_tab; i++){
+		if ( tab[i] != -52 ){
+			printf("%c", tab[i]);
+		}
+	}
 	printf("\n");
+
+	Cesar(tab, move, max_tab);
+	printf("String(crypted): ");
+	for( i = 0; i < max_tab; i++){
+		if ( tab[i] != -52 ){
+			printf("%c", tab[i]);
+		}
+	}
+	printf("\n");
+
+	CesarDecryption(tab, move, max_tab);
+	printf("String(decrypted): ");
+	for( i = 0; i < max_tab; i++){
+		if ( tab[i] != -52 ){
+			printf("%c", tab[i]);
+		}
+	}
+	printf("\n");
+
 	system("PAUSE");
 	return EXIT_SUCCESS;
 }
