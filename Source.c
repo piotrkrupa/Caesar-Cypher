428ba14795d4c9565c78f89e7e4ea7200e3c99bd
pitjdm <piotr.krupa91@gmail.com>
2015-04-28 11:16:29 +0200

    beta v1.0

--- Source.c
+++ Source.c
@@ -9,17 +9,20 @@
 void Cesar(char *tab, int move, int size){
 	int i, temp;
 	for( i = 0; i < size; i++){
-		if( tab[i] >= 97 && tab[i] <= 122 ){
-			if( tab[i] >= 97 && tab[i] <= 122 ){
-				tab[i] = tab[i] + move; // temp - probability of ASCII overflow (only 0-127)
+		if( tab[i] >= 97 && tab[i] <= 122 ){ //for small letters
+				temp= tab[i];   //int temp - probability of ASCII overflow (0-127)
+			if( temp >= 97 && temp <= 122 ){
+				temp = temp + move; 
+				if( temp <= 122 ){
+					tab[i]=temp;
+				}
 			}
-			if( tab[i] > 122){
-				tab[i] = tab[i] - 26;
+			if( temp > 122){
+				temp = temp - 26;
+				tab[i]=temp;
 			}
-
 		}
-
-		if( tab[i] >= 65 && tab[i] <= 90 ){
+		if( tab[i] >= 65 && tab[i] <= 90 ){ //for big letters
 			if( tab[i] >= 65 && tab[i] <= 90 ){
 				tab[i] = tab[i] + move;
 			}
@@ -32,7 +35,7 @@ void Cesar(char *tab, int move, int size){
 void CesarDecryption(char *tab, int move, int size){
 	int i;
 	for( i = 0; i < size; i++){
-		if( tab[i] >= 97 && tab[i] <= 122 ){
+		if( tab[i] >= 97 && tab[i] <= 122 ){ //for small letters
 			if( tab[i] >= 97 && tab[i] <= 122 ){
 				tab[i] = tab[i] - move;
 			}
@@ -40,7 +43,7 @@ void CesarDecryption(char *tab, int move, int size){
 				tab[i] = tab[i] + 26;
 			}
 		}
-		if( tab[i] >= 65 && tab[i] <= 90 ){
+		if( tab[i] >= 65 && tab[i] <= 90 ){ //for big letters
 			if( tab[i] >= 65 && tab[i] <= 90 ){
 				tab[i] = tab[i] - move;
 			}
@@ -55,9 +58,8 @@ int main(){
 	int p, move, i;
 	char tab[max_tab], *ptab;
 	
-	p=27; //move factor, p>0 !
-	//move= p%26; 
-	move=18;
+	p = 20000; //move factor, p>0 !
+	move = p%26; 
 	printf("p: %d\nmove: %d\n", p, move);
 
 	ptab = fgets(tab, max_tab, stdin);
